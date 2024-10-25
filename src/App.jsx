e66b2a11ab7a061e7024595978398718769345cd
realpolya <ps2780@columbia.edu>
2024-10-25 16:19:59 -0400

    navbar and sign in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
+/* --------------------------------Imports--------------------------------*/
+
+import { useState, useEffect, createContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
+// component imports below
+import NavBar from './components/NavBar/NavBar.jsx';
+
+/* --------------------------------Variables--------------------------------*/
+
+// AuthContext can be set to an object (if you want to pass down
+// multiple items through the value prop)
+// EXAMPLE: const contextObject = { user, setUser }
+const AuthContext = createContext(null);
+
+/* --------------------------------Function--------------------------------*/
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [user, setUser] = useState(null);
+
+  const authObject = { user, setUser }
 
   return (
     <>
-      <h1>Savor the Seasons</h1>
+      <AuthContext.Provider value={authObject}>
+        
+        < NavBar handleSignOut={handleSignOut} />
+
+        <h1>Savor the Seasons</h1>
+
+        <Routes>
+
+          { user ? (< Route path="/" element={<Dashboard/>} />) : (< Route path="/" element={<Landing />} />)}
+          < Route path="/sign-up" element={< SignupForm setUser={setUser}/>} />
+          < Route path="/sign-in" element={< SigninForm setUser={setUser}/>} />
+          
+        </Routes>
+
+        < Footer />
+
+      </AuthContext.Provider>
     </>
   )
-}
 
-export default App
+}
+/* --------------------------------Exports--------------------------------*/
+
+export { AuthContext };
+export default App;
---
+++ src/components/SignInForm/SignInForm.jsx
@@ -0,0 +1,28 @@
+/* --------------------------------Imports--------------------------------*/
+
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+import { useContext } from 'react';
+
+// contexts & services
+import { AuthContext } from '../../App.jsx';
+
+// css
+import './SignInForm.css';
+
+/* --------------------------------Function--------------------------------*/
+
+function SignInForm() {
+
+    const {setUser} = useContext(AuthContext);
+
+    return (
+      <main>
+          SignInForm
+      </main>
+    )
+  }
+
+/* --------------------------------Exports--------------------------------*/
+
+export default SignInForm
