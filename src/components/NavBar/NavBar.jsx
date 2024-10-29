58fb0085d82bfdaafea734ea899744a330813ce2
realpolya <ps2780@columbia.edu>
2024-10-29 13:48:52 -0400

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,56 @@
 /* --------------------------------Imports--------------------------------*/
 import { Link } from 'react-router-dom'
-import './NavBar.css'
 import { AuthContext } from '../../App'
 import { useContext } from 'react'
+
+import { signOut } from '../../services/authService.js'
+
+import './NavBar.css'
 /* --------------------------------Function--------------------------------*/
 
 function NavBar() {
+
+  const {user, setUser} = useContext(AuthContext);
+
+  const handleSignOut = () => {
+    signOut();
+    setUser(null);
+  }
+
+  const userNav = <ul id="nav-bar-ul">
+      <li>
+        <Link to="/">Recipes</Link>
+      </li>
+      <li>
+        <Link to="/recipe-form">New Recipe</Link>
+      </li>
+      <li>
+        <Link to="/" onClick={handleSignOut}>Sign out</Link>
+      </li>
+      <li>
+        <Link to="/about-team">About</Link>
+      </li>
+  </ul>
+
+  const outNav = <ul id="nav-bar-ul">
+      <li>
+        <Link to="/sign-up">Sign up</Link>
+      </li>
+      <li>
+        <Link to="/sign-in">Sign in</Link>
+      </li>
+      <li>
+        <Link to="/about-team">About</Link>
+      </li>
+  </ul>
+
   return (
     <nav id='nav-bar'>
-      <ul>
-        <li>
-          <Link to="/">DashBoard</Link>
-          <Link to="//sign-up">sign up</Link>
-          <Link to="/sign-in">sign in</Link>
-          <Link to="/about-team"> about team</Link>
-          
-        </li>
-      </ul>
+
+      <h1 id="nav-bar-h1">Savor the Seasons</h1>
+
+      {user ? userNav : outNav}
+      
     </nav>
   )
 }
