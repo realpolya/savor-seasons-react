156572a77a24c849c810b3c57aafcb19e22c8c35
realpolya <ps2780@columbia.edu>
2024-10-30 07:59:15 -0400

    work on review list and css

--- src/components/RecipePage/ReviewsList/ReviewsList.jsx
+++ src/components/RecipePage/ReviewsList/ReviewsList.jsx
@@ -7,16 +7,33 @@ import './ReviewsList.css';
 
 /* --------------------------------Function--------------------------------*/
 
-function ReviewsList() {
+function ReviewsList({ recipe }) {
 
     return (
+
       <section className="reviews-list">
-        <div className="review">
-          <p className="review-text">
-            
-          </p>
-          </div>
+
+        <h2 id="reviews-list-h2">Reviews</h2>
+
+        {recipe.reviews.map(review => {
+          return <div className="review-div">
+              <h3 className="review-h3">{review.name}</h3>
+              <div className="review-author-rating">
+                <p><span>by</span> {review.reviewer}</p>
+                <div className="review-rating-div">
+                  <p className="review-rating-text">{review.rating} out of 5</p>
+                  < RatingsReviews rating={review.rating}/>
+                </div>
+              </div>
+              <p className="review-text">
+                {review.text}
+              </p>
+            </div>
+        })}
+
+
       </section>
+
     )
 
   }
@@ -24,3 +41,17 @@ function ReviewsList() {
 /* --------------------------------Exports--------------------------------*/
 
 export default ReviewsList;
+
+// name: {
+//   type: String,
+//   required: true,
+// },
+// text: {
+//   type: String,
+//   required: true,
+// },
+// rating: {
+//   type: Number,
+//   required: true,
+//   enum: [1, 2, 3, 4, 5],
+// },
