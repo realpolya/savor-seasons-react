5d8ced65ec664b2dd366c917ba8042a1b1bb7d5f
realpolya <ps2780@columbia.edu>
2024-10-31 18:45:00 -0400

    log in and sign up css

--- src/components/Dashboard/MyFavoritesTable/MyFavoritesTable.jsx
+++ src/components/Dashboard/MyFavoritesTable/MyFavoritesTable.jsx
@@ -1,7 +1,7 @@
 /* --------------------------------Imports--------------------------------*/
 
 import {Link} from 'react-router-dom';
-import {useContext, useState} from 'react';
+import {useContext} from 'react';
 
 import {AuthContext} from '../../../App.jsx';
 
